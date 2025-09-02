cf711888dd770fb4965372977c26e0d97e620c6b
yjshin.cloud <yjshin.cloud@gmail.com>
2025-09-02 14:10:21 +0900

    Initial commit

---
+++ src/main/java/com/example/springrest2/Springrest2Application.java
@@ -0,0 +1,13 @@
+package com.example.springrest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springrest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springrest2/Springrest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springrest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
